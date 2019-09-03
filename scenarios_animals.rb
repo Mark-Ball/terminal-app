f421e1d81011d2cebc8d101df30b99d496cf0c07
Mark-Ball <markball111@gmail.com>
2019-09-04 05:46:08 +1000

    written list of scenarios and animals

---
+++ scenarios_animals.rb
@@ -0,0 +1,35 @@
+#array of scenarios
+#scenario 1: bravery
+#scenario 2: introversion
+scenario_1 = "You and your friend are at the bar. Your friend accidentally spills his drink on a group of large drunk men. They demand an apology but your friend refuses. An argument begins. He squares up as if to fight them all, then looks to you. What do you do? 
+        Options: 
+        1. Stand with your friend no mattery what
+        2. Convince your friend to back down
+        3. What friend? I don't even know this guy!"
+
+scenario_2 = "You are at a party with a few friends, and many people you don't know. What do you do?
+        Options:
+        1. Hang out with the people you already know
+        2. Meet some new people"
+
+scenario_3 = "You are playing a casual game of mario kart with your spouse. You are winning until you are hit by a blue shell right before the finish line and end up losing. Your spouse points at you, laughs, and jumps up in glee. What do you do?
+    Options:
+    1. Demand a rematch to reclaim your honour
+    2. Let your spouse known that gloating is not an appropriate way to celebrate a win
+    3. Congratulate them on their win"
+
+scenario_4 = "Your neighbour plays music loudly in the middle of the night. It disturbs your sleep and you are unable to focus in the next day's Ruby class, getting many answers wrong in the Kahoot. This continues for several nights. What do you do?
+    Options:
+    1. Knock on your neighbour's door to ask then face-to-face to stop
+    2. Leave a note asking them to stop
+    3. Accept it and buy some earplugs"
+
+animals = [
+    "Norwegian lemming", #aggressive, group
+    "Capybara", #friendly, group
+    "Moose", #aggressive, individual
+    "Koala", #friendly, individual
+
+
+    
+]
